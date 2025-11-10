81c8a53c6d99d232f5af38adb5945fb9ade0b818
Mag_Mau <114644065+mauerhofer4@users.noreply.github.com>
2025-11-10 07:10:06 +0000

    Round 1

--- A1_ZweiThreadsZaehlenWinner/Program.cs
+++ A1_ZweiThreadsZaehlenWinner/Program.cs
@@ -11,16 +11,32 @@ class Program
     {
         Console.WriteLine("Übung 1: Zwei Threads – Zählen & Winner");
         
+        Thread threadA = new Thread(CountUpThreadA);
+        Thread threadB = new Thread(CountDownThreadB);
+
+        threadA.Start();
+        threadB.Start();
+        
+        threadA.Join();
+        threadB.Join();
         
     }
     
     private static void CountUpThreadA()
     {
-        
+        for(int i = 0; 1 <= 0; i++)
+        {
+            Console.WriteLine($"Thread A: {i}")
+            Thread.Sleep(100);
+        }
     }
     
     private static void CountDownThreadB()
     {
-       
+       for(int i = 100; 1 >= 0; i--)
+        {
+            Console.WriteLine($"Thread B: {i}")
+            Thread.Sleep(100);
+        }
     }
 }
