dd59331cc147ee4f1aced91fe7b9b2882ceac564
Mag_Mau <114644065+mauerhofer4@users.noreply.github.com>
2025-11-10 07:36:55 +0000

    bank

--- A2_RaceConditionBank/Program.cs
+++ A2_RaceConditionBank/Program.cs
@@ -13,12 +13,23 @@ class Program
         // Bankkonto mit Startwert 1000 EUR erstellen
         BankAccount account = new BankAccount(1000);
         Console.WriteLine($"Startkontostand: {account.GetBalance()} EUR\n");
+
+        for(int i = 0; i<11; i++)
+        {
+            Thread thread = new Thread(() => PerformBankOperations(account));
+            thread.Start();
+            thread.Join();
+
+            Console.WriteLine($"Kontostand nach Transaktion {i + 1}: {account.GetBalance()} EUR");
+        }
         
     }
     
     private static void PerformBankOperations(BankAccount account)
     {
-        
+        account.Deposit(100);
+        Thread.Sleep(100);
+        account.Withdraw(150);
     }
 }
 
